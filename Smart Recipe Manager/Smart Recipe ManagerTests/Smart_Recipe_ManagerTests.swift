48abc0c3910d438e4b46f8d4d1bba946c3602de2
Gaurang Batra <155742509+Gaurang-Batra@users.noreply.github.com>
2024-11-23 11:50:21 +0530

    Add files via upload

---
+++ Smart Recipe Manager/Smart Recipe ManagerTests/Smart_Recipe_ManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_Recipe_ManagerTests.swift
+//  Smart Recipe ManagerTests
+//
+//  Created by Gaurang  on 23/11/24.
+//
+
+import Testing
+@testable import Smart_Recipe_Manager
+
+struct Smart_Recipe_ManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
